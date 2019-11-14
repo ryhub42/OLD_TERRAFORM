c8601fd6551a1a3ad7cd0e4b024b960d5b84d6c3
Rajani y <ryhub42@gmail.com>
2019-11-14 15:06:26 -0500

    new

--- 02-Resources/ec2.tf
+++ 02-Resources/ec2.tf
@@ -1,7 +1,19 @@
 resource "aws_instance" "web" {
   ami = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
-tags = {
-    Name = "HelloWorld"
+  vpc_security_group_ids = ["sg-08bed8525f85d783e", "sg-0f8ef2eb8f766f7d1"]
+
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "root"
+      password = "DevOps321"
+      host = self.public_ip
+    }
+    inline = [
+      "cd /tmp",
+      "git clone https://github.com/ryhub42/Shell-Scripts.git",
+      "sh /tmp/shell-scripts/studentapp/install.sh",
+    ]
   }
 }
