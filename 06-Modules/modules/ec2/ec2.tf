76b1d2ee121d44f68a4433928181059aa7446f64
Rajani y <ryhub42@gmail.com>
2019-11-15 16:36:37 -0500

    new

--- 06-Modules/modules/ec2/ec2.tf
+++ 06-Modules/modules/ec2/ec2.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
+  tags          = {
+    Name        = "${var.tags["project_name"]}-${var.tags["env"]}}"
+  }
 }
 
