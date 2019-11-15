cea991e30e4162baec25aa0df3181db6a88a0b72
Rajani y <ryhub42@gmail.com>
2019-11-15 11:29:32 -0500

    new

---
+++ 04-variables/04-lists/main.tf
@@ -0,0 +1,10 @@
+variable "sample" {
+  type = "list"
+  default = ["sg1", "sg2", "sg3", 10, true]
+}
+
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo ${var.sample}"
+  }
+}
