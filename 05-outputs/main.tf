c18fecc5737703e3710c27e519fb1e4b6130a6a9
Rajani y <ryhub42@gmail.com>
2019-11-15 15:20:25 -0500

    new

--- 05-outputs/main.tf
+++ 05-outputs/main.tf
@@ -1,16 +1,6 @@
-variable "ami"{
-  default = "ami-02383135f96b2293e"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
 
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
 }
 
-output "PUBLIC_IP" {
-  value = aws_instance.web.public_ip
-}
---
+++ 05-outputs/outupts.tf
@@ -0,0 +1,3 @@
+output "PUBLIC_IP" {
+  value = aws_instance.web.public_ip
+}
