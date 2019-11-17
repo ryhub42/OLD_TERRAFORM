7db3a18b460b9124371170d068ce374943917f7b
Rajani y <ryhub42@gmail.com>
2019-11-17 18:22:42 -0500

    new

--- PROJECT-01/modules/rds/rds.tf
+++ PROJECT-01/modules/rds/rds.tf
@@ -1,8 +1,8 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = var.RDS_DB_SIZE
   storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
+  engine               = "mariadb"
+  engine_version       = "10.3"
   instance_class       = "var.RDS_INSTANCE_TYPE"
   name                 = "var.RDS_DBNAME"
   username             = "var.RDS_USERNAME"
