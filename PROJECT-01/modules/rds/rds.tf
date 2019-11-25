f0a2b39a940e015daffcd01821e4823c5d08b5b2
Rajani y <ryhub42@gmail.com>
2019-11-25 14:58:38 -0500

    1

--- PROJECT-01/modules/rds/rds.tf
+++ PROJECT-01/modules/rds/rds.tf
@@ -3,10 +3,10 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mariadb"
   engine_version       = "10.3"
-  instance_class       = "var.RDS_INSTANCE_TYPE"
-  name                 = "var.RDS_DBNAME"
-  username             = "var.RDS_USERNAME"
-  password             = "var.RDS_PASSWORD"
+  instance_class       = var.RDS_INSTANCE_TYPE
+  name                 = var.RDS_DBNAME
+  username             = var.RDS_USERNAME
+  password             = var.RDS_PASSWORD
   parameter_group_name = "default.mariadb10.3"
   identifier           = "${lower(var.tags["project_name"])}-${lower(var.tags["env"])}-mariadb"
 }
