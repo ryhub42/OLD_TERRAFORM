ad79b92fc77e1ee0472b219eb36ae663e945a05f
Rajani y <ryhub42@gmail.com>
2019-11-29 16:57:28 -0500

    1

--- 07-loops/07-loops.tf
+++ 07-loops/07-loops.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "web" {
   ami            = "ami-02383135f96b2293e"
   instance_type  = "t2.micro"
   tags           = {
-    Name = "ansible-node"
+    Name = "Ansible-Node"
   }
 }
